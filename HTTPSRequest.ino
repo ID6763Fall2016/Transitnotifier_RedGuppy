28ecb683b692243e2a0a510d4bc838e1e79280e4
Edith Li <abenoy05@gmail.com>
2016-10-11 23:29:09 -0400

    Add files via upload

---
+++ HTTPSRequest.ino
@@ -0,0 +1,74 @@
+/*
+ *  HTTP over TLS (HTTPS) example sketch
+ *
+ *  This example demonstrates how to use
+ *  WiFiClientSecure class to access HTTPS API.
+ *  We fetch and display the status of
+ *  esp8266/Arduino project continuous integration
+ *  build.
+ *
+ *  Created by Ivan Grokhotkov, 2015.
+ *  This example is in public domain.
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "1412";
+const char* password = "chacha12";
+
+const char* host = "gtbuses.herokuapp.com";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+ 
+void setup() {
+  int c;
+  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+ 
+
+  String url = "/predictions/trolley";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+  
+ client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+               
+  Serial.println("request sent");
+  while (client.connected()) {
+    if (client.available()) {
+      String line = client.readStringUntil('\n');
+      Serial.print(line);
+    }
+  }
+  
+ 
+  Serial.println("==========");
+  Serial.println("closing connection");
+}
+
+void loop() {
+}
