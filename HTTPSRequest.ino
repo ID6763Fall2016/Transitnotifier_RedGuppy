16623975379bbf996122e041ee6f0a846365ec79
Edith Li <abenoy05@gmail.com>
2016-10-11 23:53:03 -0400

    Update HTTPSRequest.ino
    add parser

--- HTTPSRequest.ino
+++ HTTPSRequest.ino
@@ -13,6 +13,7 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#include <TinyXML.h>
 
 const char* ssid = "1412";
 const char* password = "chacha12";
@@ -20,13 +21,18 @@ const char* password = "chacha12";
 const char* host = "gtbuses.herokuapp.com";
 const int httpsPort = 443;
 
+TinyXML    xml;
+uint8_t    buffer[150]; // For XML decoding
+
 // Use web browser to view and copy
  
 void setup() {
   int c;
   
   Serial.begin(115200);
-  Serial.println();
+  Serial.println("GTBus Tracker");
+  xml.init((uint8_t *)buffer, sizeof(buffer), &XML_callback);
+
   Serial.print("connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -53,15 +59,17 @@ void setup() {
   Serial.print("requesting URL: ");
   Serial.println(url);
   
- client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Connection: close\r\n\r\n");
                
   Serial.println("request sent");
   while (client.connected()) {
-    if (client.available()) {
-      String line = client.readStringUntil('\n');
-      Serial.print(line);
+    if (client.available() > 0) {
+      char c = client.read();
+      xml.processChar(c);
+      Serial.print(c);
     }
   }
   
@@ -71,4 +79,51 @@ void setup() {
 }
 
 void loop() {
+}
+
+void XML_callback( uint8_t statusflags, char* tagName,  uint16_t tagNameLen,  char* data,  uint16_t dataLen )
+{
+
+  if ((statusflags & STATUS_ATTR_TEXT) && !strcasecmp(tagName, "seconds")) {
+    uint32_t t = atoi(data); // Prediction in seconds (0 if gibberish)
+    Serial.print(t);
+    Serial.println(" seconds");
+  }
+
+  if (statusflags & STATUS_START_TAG)
+  {
+    if ( tagNameLen )
+    {
+      Serial.print("Start tag ");
+      Serial.println(tagName);
+    }
+  }
+  else if  (statusflags & STATUS_END_TAG)
+  {
+    Serial.print("End tag ");
+    Serial.println(tagName);
+  }
+  else if  (statusflags & STATUS_TAG_TEXT)
+  {
+    Serial.print("Tag:");
+    Serial.print(tagName);
+    Serial.print(" text:");
+    Serial.println(data);
+  }
+  else if  (statusflags & STATUS_ATTR_TEXT)
+  {
+    Serial.print("Attribute:");
+    Serial.print(tagName);
+    Serial.print(" text:");
+    Serial.println(data);
+  }
+  else if  (statusflags & STATUS_ERROR)
+  {
+    Serial.print("XML Parsing error  Tag:");
+    Serial.print(tagName);
+    Serial.print(" text:");
+    Serial.println(data);
+  }
+
+
 }
