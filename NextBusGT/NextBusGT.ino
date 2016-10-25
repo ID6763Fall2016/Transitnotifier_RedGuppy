77c7b7861b10e6ae712a1f9c5d104653ac7b1e12
colinfizgig <colinfizgig@gmail.com>
2016-10-25 00:35:45 -0400

    Added hooks for button pushes to swap routes

--- NextBusGT/NextBusGT.ino
+++ NextBusGT/NextBusGT.ino
@@ -20,7 +20,7 @@
 #define NUMPIXELS      60    // Number of Pixels on the Ring
 #define BRIGHTNESS     30    // Brightness between 0 - 255
 
-byte pixelColorRed, pixelColorGreen, pixelColorBlue;
+byte pixelColorRed, pixelColorGreen, pixelColorBlue, storePixRed, storePixGreen, storePixBlue;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -43,11 +43,22 @@ int myCount = 0;
 
 int myPredictions[10];
 
+// setup button pin
+const int  buttonPin = 15; 
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
 // Use web browser to view and copy
      // Use WiFiClientSecure class to create TLS connection
   WiFiClientSecure client;
   
 void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  
   int c;
   
   Serial.begin(115200);
@@ -71,12 +82,62 @@ void setup() {
   
   pixels.setBrightness(BRIGHTNESS);
 
+  storePixRed = 170;
+  storePixGreen = 100;
+  storePixBlue = 0;
+
   parseServer(serverDelay);
 }
 
 uint8_t myCountdown = 0;
 
 void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+ 
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      if(buttonPushCounter <= 3) {
+        buttonPushCounter++;
+        Serial.println("on");
+        Serial.print("number of button pushes:  ");
+        Serial.println(buttonPushCounter);
+        if(buttonPushCounter == 1) {
+          storePixRed = 170;
+          storePixGreen = 100;
+          storePixBlue = 0;
+        }else if(buttonPushCounter == 2) {
+          storePixRed = 0;
+          storePixGreen = 0;
+          storePixBlue = 200;
+        }else if(buttonPushCounter == 3) {
+          storePixRed = 170;
+          storePixGreen = 0;
+          storePixBlue = 170;
+        }else if(buttonPushCounter > 3) {
+          storePixRed = 170;
+          storePixGreen = 100;
+          storePixBlue = 0;
+          buttonPushCounter = 0;
+        }
+      }
+      
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+  
 
   // get the estimates for the next bus and place them value in the myPredictions array
   parseServer(serverDelay);
@@ -94,16 +155,16 @@ void loop() {
        
           if(60-myPredictions[0] < (60-1)){
             myCountdown = 0;
-            if (myPredictions[j] != 0 && i >= (60-myPredictions[j])-2 && i <= (60-myPredictions[j]) + 2) {
-              pixelColorRed = 170;
-              pixelColorGreen = 100; 
-              pixelColorBlue = 0; 
+            if (myPredictions[j] != 0 && i >= (60-myPredictions[j])-1 && i <= (60-myPredictions[j]) + 1) {
+              pixelColorRed = storePixRed;
+              pixelColorGreen = storePixGreen; 
+              pixelColorBlue = storePixBlue; 
             }
           }else{
               if(i > myCountdown) {
-                pixelColorRed = 170;
-                pixelColorGreen = 100; 
-                pixelColorBlue = 0;
+                pixelColorRed = storePixRed;
+                pixelColorGreen = storePixGreen; 
+                pixelColorBlue = storePixBlue;
               }else {
                 pixelColorRed = 0;
                 pixelColorGreen = 0; 
@@ -141,15 +202,38 @@ void parseServer(int myDelay){
 
     //String url = "/multiPredictions?stops=night|cloucomm";
     Serial.print("requesting URL: ");
-    const char* url = trolleyString;
-    Serial.println(url);
-  
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                "Host: " + host + "\r\n" +
-                "User-Agent: trolleyStopClockESP8266\r\n" +
-                "Connection: close\r\n\r\n");
-               
-    Serial.println("request sent");
+    if(buttonPushCounter <= 1 || buttonPushCounter > 3) {
+      const char* url = trolleyString;
+      Serial.println(url);
+    
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                  "Host: " + host + "\r\n" +
+                  "User-Agent: trolleyStopClockESP8266\r\n" +
+                  "Connection: close\r\n\r\n");
+                 
+      Serial.println("request sent");
+    }else if (buttonPushCounter == 2) {
+      const char* url = techString;
+      Serial.println(url);
+    
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                  "Host: " + host + "\r\n" +
+                  "User-Agent: trolleyStopClockESP8266\r\n" +
+                  "Connection: close\r\n\r\n");
+                 
+      Serial.println("request sent");
+    }else if (buttonPushCounter == 3) {
+      const char* url = nightString;
+      Serial.println(url);
+    
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                  "Host: " + host + "\r\n" +
+                  "User-Agent: trolleyStopClockESP8266\r\n" +
+                  "Connection: close\r\n\r\n");
+                 
+      Serial.println("request sent");
+    }
+    
    while (client.connected()) {
       if (client.available() > 0) {
         char c = client.read();
